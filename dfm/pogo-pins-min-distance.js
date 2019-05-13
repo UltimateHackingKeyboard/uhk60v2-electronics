3146a8246fbad8870be016a2e1a0ab3421d3152e
László Monda <laci@monda.hu>
2019-05-13 20:46:11 +0200

    Use const wherever possible in pogo-pins-distance.js

--- dfm/pogo-pins-min-distance.js
+++ dfm/pogo-pins-min-distance.js
@@ -1,55 +1,56 @@
 #!/usr/bin/env node
 
-let fs = require('fs');
-let path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 function extractTestPoints(filename) {
-    let fileContent = fs.readFileSync(filename, {encoding:'utf8'});
+    const fileContent = fs.readFileSync(filename, {encoding:'utf8'});
+    const testPoints = [];
     let referenceDesignator;
-    let testPoints = [];
     let isInsideTestPoint = false;
 
     fileContent
     .split('\n')
     .forEach((line) => {
-        let matches = line.match(/^\$MODULE \"TP_(.+)"/);
+        const matches = line.match(/^\$MODULE \"TP_(.+)"/);
         if (matches) {
             referenceDesignator = matches[1];
             isInsideTestPoint = true;
         }
-        matches = line.match(/^position ([0-9-.]+) ([0-9-.]+) /);
-        if (matches && isInsideTestPoint) {
+
+        const matches2 = line.match(/^position ([0-9-.]+) ([0-9-.]+) /);
+        if (matches2 && isInsideTestPoint) {
             testPoints.push({
                 referenceDesignator: referenceDesignator,
-                x: +matches[1],
-                y: +matches[2]
+                x: +matches2[1],
+                y: +matches2[2],
             });
             isInsideTestPoint = false;
         }
-    })
+    });
 
     return testPoints;
 };
 
 function computeTestPointDistances(testPoints) {
-    let distances = [];
+    const distances = [];
     let i = 0;
     testPoints.forEach(testPoint1 => {
         testPoints.slice(++i, testPoints.length).forEach(testPoint2 => {
             distances.push({
                 points: `${testPoint1.referenceDesignator} - ${testPoint2.referenceDesignator}`,
-                distance: Math.sqrt(Math.pow(testPoint1.x-testPoint2.x, 2) + Math.pow(testPoint1.y-testPoint2.y, 2))
+                distance: Math.sqrt(Math.pow(testPoint1.x-testPoint2.x, 2) + Math.pow(testPoint1.y-testPoint2.y, 2)),
             });
-        })
-    })
+        });
+    });
     return distances;
 };
 
 ['../left-main/left-main.rpt', '../right-main/right-main.rpt', '../display/display.rpt'].forEach(filename => {
-    let testPoints = extractTestPoints(filename);
-    let testPointDistances = computeTestPointDistances(testPoints);
+    const testPoints = extractTestPoints(filename);
+    const testPointDistances = computeTestPointDistances(testPoints);
     testPointDistances.sort((a, b) => a.distance < b.distance ? -1 : 1);
     fs.writeFileSync(
         path.basename(filename).replace(/\.[^/.]+$/, '') + '.txt',
         testPointDistances.map(testPointDistance => `${testPointDistance.points}: ${testPointDistance.distance}`).join('\n'));
-})
+});

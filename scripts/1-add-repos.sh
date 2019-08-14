13e1f5b410237e5b02141a6d7adf59356d90d12d
László Monda <laci@monda.hu>
2019-08-15 00:09:22 +0200

    Remove scripts directory.

--- scripts/1-add-repos.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Adding repos"
-sudo add-apt-repository --yes ppa:js-reynaud/kicad-4
-sudo add-apt-repository --yes ppa:saiarcot895/myppa
-echo "Running apt-get update"
-sudo apt-get update -qq
--- scripts/2-install-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Installing packages"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-fast #http://serverfault.com/q/227190/169180
-sudo apt-fast install -y kicad git imagemagick curl poppler-utils
